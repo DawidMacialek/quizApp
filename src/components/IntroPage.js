8e720b702a212df8f84df5ab060f1db2809cc040
Dawid Macialek <macialekdawid@gmail.com>
2022-01-20 20:15:34 +0100

    controlled input

--- src/components/IntroPage.js
+++ src/components/IntroPage.js
@@ -1,24 +1,47 @@
 import React, { useState } from 'react';
 
-export default function IntroPage() {
+export default function IntroPage(props) {
   return (
-    <main className='container is-fullhd has-background-primary'>
+    <main className='intro--container'>
       <h1>Quiz App</h1>
       <form>
-        <select name='category' id='category'>
+        <select
+          value={props.quizSettings.category}
+          onChange={props.handleChangeSettings}
+          name='category'
+          id='category'
+        >
           <option value='any category'>Any category</option>
           <option value='general knowladge'>General knowladge</option>
+          <option value='sports'>Sports</option>
+          <option value='history'>History</option>
+          <option value='geography'>Geography</option>
+          <option value='politicts'>Politics</option>
           <option value='animal'>Animal</option>
           <option value='art'>Art</option>
         </select>
-        <select name='difficulty' id='difficulty'>
+        <select
+          name='level'
+          onChange={props.handleChangeSettings}
+          value={props.quizSettings.level}
+          id='difficulty'
+        >
           <option value='easy'>Easy</option>
           <option value='medium'>Medium</option>
           <option value='hard'>Hard</option>
         </select>
         <label htmlFor='numberOfQuestions'>Number Of Questions (max=40)</label>
-        <input type='number' id='numberOfQuestions' max='40' min='1' />
+        <input
+          onChange={props.handleChangeSettings}
+          value={props.quizSettings.numOfQuest}
+          name='numOfQuest'
+          type='number'
+          id='numberOfQuestions'
+          max="40"
+          min='1'
+        />
       </form>
+      <button className='intro--btn'>Let's Play!</button>
     </main>
   );
 }
